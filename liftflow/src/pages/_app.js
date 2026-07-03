12a2d4077d21c156dd77469b4ee6c561fceb9171
Peyton <legolightning71@gmail.com>
2026-07-03 17:04:25 -0600

    Improve UI and add new features

--- liftflow/src/pages/_app.js
+++ liftflow/src/pages/_app.js
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.register("/sw.js").catch(() => {});
+    }
+  }, []);
+
   return (
     <Layout>
       <Component {...pageProps} />
--- liftflow/src/pages/_document.js
+++ liftflow/src/pages/_document.js
@@ -3,7 +3,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="application-name" content="LiftFlow" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+        <meta name="apple-mobile-web-app-title" content="LiftFlow" />
+        <meta name="description" content="LiftFlow workout tracking app" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#32cfff" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
